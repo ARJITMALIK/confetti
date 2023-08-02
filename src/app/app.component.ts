891a47933b6e121dc827f47e3702093cd2fdcbcc
ARJITMALIK <arjitmalik2001@gmail.com>
2023-08-02 15:22:26 +0530

    elden ring styled buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'confetti';
+
+  public selectSoundEffect = new Audio();
+
+  ngAfterViewInit(): void {
+    this.selectSoundEffect.src = '../assets/sounds/select.mp3';
+    this.selectSoundEffect.load();
+    this.selectSoundEffect.currentTime = 0;
+  }
+
+  onMouseHover(): void {
+    this.selectSoundEffect.currentTime = 0.1;
+
+    this.selectSoundEffect.play();
+  }
 }
